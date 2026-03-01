7ef25ac4ce409fdc01e340543cbe17efafeb317a
lishanlung <cnjlwitter@gmail.com>
2026-03-01 15:30:49 +0000

    implemented scraper + inventory in main

--- Book Vending Machine/main.py
+++ Book Vending Machine/main.py
@@ -1,10 +1,24 @@
-from book_vending_machine import Book, BookScraper, Inventory, Basket, PurchaseManager
+from book_vending_machine import BookScraper, Book, Inventory
+import random
+from rich.console import Console
+from rich.theme import Theme
 
 try:
+    console = Console(
+        theme=Theme({"markdown.block_quote": "dim"})
+        )
+    
     scraper = BookScraper()
-    all_books = scraper.get_all_books()
-    inventory = Inventory(all_books)
 
-    print(inventory.books)
+    with console.status("Loading stock info and building inventory", spinner="aesthetic") as status:
+        pass
+        all_books: list[Book] = scraper.get_all_books()
+        
+        #grab 5 example books and print    
+        example_inventory: list[Book] = random.sample(all_books, 5)
+
+    inventory = Inventory(example_inventory)
+    inventory.print_stock_and_details(console)
+
 except Exception as e:
     print("Error:", e, e.__traceback__.tb_lineno)
