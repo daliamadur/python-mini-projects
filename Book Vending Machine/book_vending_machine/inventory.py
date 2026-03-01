1722e992e6ef05b0df081881c2033d3a7c00743e
lishanlung <cnjlwitter@gmail.com>
2026-03-01 15:31:26 +0000

    implemented book inventory class

--- Book Vending Machine/book_vending_machine/inventory.py
+++ Book Vending Machine/book_vending_machine/inventory.py
@@ -1,30 +1,51 @@
+from rich.table import Table
+from rich.markdown import Markdown
+from rich.console import Console
+from .models import Book
+
 class Inventory():
-    def __init__(self, books = []):
-        self.books = books
+    def __init__(self, inventory: list[Book]):
+        self.inventory = inventory
 
-    def add_book(self, book):
-        self.books.append(book)
+    def _heading(self):
+        return Markdown("# 📖 Example Stock 📖")
 
-    def get_stock_info():
-        #return every book that is in stock
-        return
+    def _rating(self, rating):
+        return f"{"[#DBB331] ★ [/#DBB331]" * rating}{"[#3E3E3E] ☆ [/#3E3E3E]" * (5 - rating)}"
 
-    def search_for_book():
-        #return books that fit criteria - category, title
-        return
-    
-    def filter_books():
-        #filter books by price, rating
-        return
-    
-    def get_book_listing():
-        #show all info related to a book with rich markdown
-        return
-    
-    def to_json():
-        #get all book info as a json file
-        return
+    def _get_stock(self):
+        table = Table(show_header=True, header_style="bold #7B1F3B")
 
-    def complete_purchase():
-        #get book from inventory, pop, and "charge" price, thank you for your purchase
-        return
+        table.add_column("Title", style="bold")
+        table.add_column("Category", style="#717171")
+        table.add_column("Rating", justify="center")
+        table.add_column("Price", justify="center")
+        table.add_column("In stock", justify="center")
+
+        for book in self.inventory:
+            table.add_row(book.title,
+                            book.category,
+                            self._rating(book.rating),
+                            f"[bold #4F9E52]£{book.price:.2f}[/bold #4F9E52]",
+                            "🗸" if book.in_stock else "[bold red]Not in stock[/bold red]")
+            
+        return table
+
+    def _get_book_details(self, book: Book):
+        details = [
+            (f"## {book.title}", True),
+            (f"🪙 **£{book.price:.2f}**", True),
+            (f"🔖 **{book.category}**", True),
+            ("[green]In Stock 🗸[/green]" if book.in_stock else "[dim]Not in stock[dim]", False),
+            (f"{self._rating(book.rating)}", False),
+            (f"> {book.description[:-8]}", True)
+        ]
+        
+        return details
+
+    def print_stock_and_details(self, console: Console):
+        console.print(self._heading())
+        console.print(self._get_stock())
+        for book in self.inventory:
+            for detail, md in self._get_book_details(book):
+                console.print(Markdown(detail) if md else detail)
